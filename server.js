7aa963f067cec1c43c687fb583e593106ad3fe13
raultocantins <alexbraul.ar@gmail.com>
2020-12-19 14:29:43 -0300

    updating route add/history

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyparser = require("body-parser");
-var connection = mongoose.connect("mongodb+srv://admin:3571592486@cluster0.ebc1x.mongodb.net/hmb?retryWrites=true&w=majority", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+var connection = mongoose.connect(
+  "mongodb+srv://admin:3571592486@cluster0.ebc1x.mongodb.net/hmb?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   history: String,
@@ -31,17 +34,38 @@ app.get("/historys", async (req, res) => {
 });
 app.post("/history", async (req, res) => {
   const id = req.body.id;
+  console.log();
   const docs = await History.findById(id, (err, doc) => {
     res.json(doc);
   });
 });
 
-app.post("add/history", (req, res) => {
+app.post("/add/history", (req, res) => {
+  const data = req.body;
+  var history = {
+    data: [],
+    history: "",
+    image: "",
+  };
+  data.map((e) => {
+    if (history.history !== "" && history.image !== "") {
+      return;
+    } else {
+      history.history = e.history;
+      history.image = e.capa;
+    }
+    var block = {
+      text: e.text,
+      trad: e.trad,
+      image: e.image,
+    };
+    history.data.push(block);
+  });
+  console.log(history);
   const novo = History.create({
-    history: "greetings",
-    image:
-      "https://d1ctrvvfkbyl4b.cloudfront.net/blog/yo@2x.png?mtime=20200729130810&focal=none",
-    data: data,
+    history: history.history,
+    image: history.image,
+    data: history.data,
   });
   res.send(novo);
 });
